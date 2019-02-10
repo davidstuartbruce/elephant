9787eb4e6a12c795a8bd4d2553855173ff24d1ad
David Bruce <davidstuartbruce@gmail.com>
2019-02-10 06:23:45 -0600

    started myscene.h/.cpp

---
+++ myscene.cpp
@@ -0,0 +1,6 @@
+#include "myscene.h"
+
+MyScene::MyScene()
+{
+
+}
---
+++ myscene.h
@@ -0,0 +1,11 @@
+#ifndef MYSCENE_H
+#define MYSCENE_H
+
+
+class MyScene : public QGraphicsScene
+{
+public:
+    MyScene();
+};
+
+#endif // MYSCENE_H
